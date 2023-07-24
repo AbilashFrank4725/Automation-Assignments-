f7464d5b3d2bfef1e6c2091439edbddc0c6def45
Abilash_Franklin_4725 <139784956+AbilashFrank4725@users.noreply.github.com>
2023-07-24 23:54:26 +0530

    Add files via upload

---
+++ Week2 Day1 Assignment/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package week2.assignments;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int firstNum=0;
+		int secNum = 1;
+		int sum = 0;
+		System.out.println(firstNum);
+		
+		for(int i=0;i<=11;i++) {
+			sum=firstNum + secNum;
+			 firstNum = secNum ;
+			 secNum = sum;
+			System.out.println(sum); 
+		}
+		
+		
+	}
+}
